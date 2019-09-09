39052f15e09cb3cfa7c87dd50fec2c776a920297
Prem Ranjan <prem_ranjan@apple.com>
2019-09-10 01:54:32 +0530

    new functionality for data display on UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: Object;
+  h1Style: boolean = false;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
   }
 
   firstClick(){
-    console.log('Clicked');
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log("this.user",this.users);
+    });
   }
 
 }
